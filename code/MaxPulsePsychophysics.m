ccd82dd054a7c36bd05247ea58da9f9fab05d92d
David Brainard <brainard@psych.upenn.edu>
2017-06-21 11:13:45 -0400

    Adopt package idea and start to draft an initial protocol
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

---
+++ code/MaxPulsePsychophysics.m
@@ -0,0 +1,17 @@
+% MaxPulsePsychophysics
+%
+% Description:
+%   Define the parameters for the MaxPulsePsychophysics protocol of the
+%   OLApproach_Psychophysics approach, and then invoke each of the
+%   functions required to set up and run the experiment.
+
+%% Clear
+clear; close all;
+
+%% Set the parameter structure here
+params.approach = 'OLApproach_Psychophysics';
+params.protocol = 'MaxPulsePsychophysics';
+params.calType = ;
+
+%% Make the nominal modulation primaries
+Psychophysics.MakeModulationNominalPrimaries;

246eecbe867b327a48e627fd1d24bc766882260c
David Brainard <brainard@psych.upenn.edu>
2017-07-05 14:52:46 -0400

    Experiment added to main script & tested. It works

--- code/+Psychophysics/Experiment.m
+++ code/+Psychophysics/Experiment.m
@@ -1,4 +1,4 @@
-function Experiment
+function protocolParams = Experiment(protocolParams)
 % Experiment
 %
 % Description
@@ -13,38 +13,32 @@ function Experiment
 % SHOULD THE ol OBJECT BE OPENED OR PASSED?  PASSED
 % I WOULD THINK.
 
-% SHOULD BE A switch on params.protocolType, so different protocols within
+% SHOULD BE A switch on params.protocol, so different protocols within
 % Psychophysics approach can do different sorts of things.
+% Update Session Log file
+protocolParams = Psychophysics.SessionLog(protocolParams,mfilename,'StartEnd','start')
 
 % Flux to the stimulus labels.
 SpeakRateDefault = getpref('OneLight', 'SpeakRateDefault');
 
 % Parameters
-params.simulate = true; % Option to run in simulate mode (true if not plugged into to a onelight box)
-% Adaptation time
-params.adaptTimeSecs = 300; % 5 minutes
-params.frameDurationSecs = 1/64;
-params.observerID = GetWithDefault('> <strong>Enter the observer name</strong>', 'MELA_xxxx');
-observerID = params.observerID;
-todayDate = datestr(now, 'mmddyy');
-params.observerAgeInYrs = GetWithDefault('> <strong>Enter the observer age?</strong>', 20);
-observerAgeInYrs = params.observerAgeInYrs;
 
-protocol = 'MaxPulsePsychophysics';
-dataPath = getpref('OneLight', 'dataPath');
-savePath = fullfile(dataPath, protocol, params.observerID, datestr(now, 'mmddyy'), 'MatFiles');
-saveFileCSV = [params.observerID '-' protocol '.csv'];
-saveFileMAT = [params.observerID '-' protocol '.mat'];
+% Adaptation time
+protocolParams.adaptTimeSecs = 300; % 5 minutes - 300
+protocolParams.frameDurationSecs = 1/64;
+savePath = fullfile(getpref(protocolParams.approach, 'DataPath'),'Experiments', protocolParams.approach, protocolParams.protocol, 'DataFiles', protocolParams.observerID, protocolParams.todayDate, protocolParams.sessionName, 'MatFiles');
+saveFileCSV = [protocolParams.observerID '-' protocolParams.protocolType '.csv'];
+saveFileMAT = [protocolParams.observerID '-' protocolParams.protocolType '.mat'];
 
 if ~exist(savePath)
     mkdir(savePath);
 end
 
 % Assemble the modulations
-modulationDir = fullfile(getpref('OneLight', 'modulationPath'));
-pathToModFileLMS = ['Modulation-MaxMelPulsePsychophysics-PulseMaxLMS_3s_MaxContrast3sSegment-' num2str(params.observerAgeInYrs) '_' observerID '_' todayDate '.mat'];
-pathToModFileMel = ['Modulation-MaxMelPulsePsychophysics-PulseMaxMel_3s_MaxContrast3sSegment-' num2str(params.observerAgeInYrs) '_' observerID '_' todayDate '.mat'];
-pathToModFileLightFlux = ['Modulation-MaxMelPulsePsychophysics-PulseMaxLightFlux_3s_MaxContrast3sSegment-' num2str(params.observerAgeInYrs) '_' observerID '_' todayDate '.mat'];
+modulationDir = fullfile(getpref(protocolParams.approach, 'ModulationStartsStopsBasePath'), protocolParams.observerID,protocolParams.todayDate,protocolParams.sessionName);
+pathToModFileLMS = ['Modulation-PulseMaxLMS_3s_MaxContrast3sSegment-' num2str(protocolParams.observerAgeInYrs) '_' protocolParams.observerID '_' protocolParams.todayDate '.mat'];
+pathToModFileMel = ['Modulation-PulseMaxMel_3s_MaxContrast3sSegment-' num2str(protocolParams.observerAgeInYrs) '_' protocolParams.observerID '_' protocolParams.todayDate '.mat'];
+pathToModFileLightFlux = ['Modulation-PulseMaxLightFlux_3s_MaxContrast3sSegment-' num2str(protocolParams.observerAgeInYrs) '_' protocolParams.observerID '_' protocolParams.todayDate '.mat'];
 
 % Load in the files
 modFileLMS = load(fullfile(modulationDir, pathToModFileLMS));
@@ -67,9 +61,8 @@ stimStopsBG = {modFileLightFlux.modulationObj.modulation.background.stops modFil
 
 % Perceptual dimensions
 perceptualDimensions = {'cool to warm', 'dull to glowing', 'colorless to colored', 'focused to blurred', 'slow to rapid', 'pleasant to unpleasant', 'dim to bright', 'smooth to jagged', 'constant to fading'};
-% Experimental stage
-params.NStimuli = length(stimOrder);
-params.NPerceptualDimensions = length(perceptualDimensions);
+protocolParams.NStimuli = length(stimOrder);
+protocolParams.NPerceptualDimensions = length(perceptualDimensions);
 
 % Wait for button press
 Speak('Press key to start experiment', [], SpeakRateDefault);
@@ -77,33 +70,32 @@ WaitForKeyPress;
 fprintf('* <strong>Experiment started</strong>\n');
 
 % Open the OneLight
-ol = OneLight('simulate',params.simulate);
+ol = OneLight('simulate',protocolParams.simulate);
 
 % Open the file to save to
 f = fopen(fullfile(savePath, saveFileCSV), 'w');
 
 trialNum = 1;
-for ii = 1:params.NStimuli
+for ii = 1:protocolParams.NStimuli
     is = stimOrder(ii);
     % Set to background
     ol.setMirrors(stimStartsBG{is}', stimStopsBG{is}');
     
     % Adapt to background for 5 minutes
-    Speak(sprintf('Adapt to background for %g minutes. Press key to start adaptation', params.adaptTimeSecs/60), [], SpeakRateDefault);
+    Speak(sprintf('Adapt to background for %g minutes. Press key to start adaptation', protocolParams.adaptTimeSecs/60), [], SpeakRateDefault);
     WaitForKeyPress;
     fprintf('\tAdaptation started.');
     Speak('Adaptation started', [], SpeakRateDefault);
     tic;
-    mglWaitSecs(params.adaptTimeSecs);
+    mglWaitSecs(protocolParams.adaptTimeSecs);
     Speak('Adaptation complete', [], SpeakRateDefault);
     fprintf('\n\tAdaptation completed.\n\t');
     toc;
     
-    for ps = 1:params.NPerceptualDimensions
+    for ps = 1:protocolParams.NPerceptualDimensions
         fprintf('\n* <strong>Trial %g</strong>\n', trialNum);
         fprintf('\t- Stimulus: <strong>%s</strong>\n', stimLabels{is});
         fprintf('\t- Dimension: <strong>%s</strong>\n', perceptualDimensions{ps});
-        %fprintf('\t- Repeat: <strong>%g</strong>\n', js);
         fprintf('\t- Repeat: <strong>%g</strong>\n');
         Speak(['Wait for instructions.'], [], 200);
         WaitForKeyPress;
@@ -111,15 +103,17 @@ for ii = 1:params.NStimuli
         keepGoing = true;
         counter = 1;
         while keepGoing
-            fprintf('* Showing stimulus...')
-            OLFlicker(ol, stimStarts{is}, stimStops{is}, params.frameDurationSecs, 1);
+            fprintf('* <strong>Showing stimulus</strong>\n')
+            OLFlicker(ol, stimStarts{is}', stimStops{is}', protocolParams.frameDurationSecs, 1);
             fprintf('Done.\n')
             counter =  counter+1;
+            ol.setMirrors(stimStartsBG{is}', stimStopsBG{is}');
             if counter == 2
                 keepGoing = GetWithDefault('Show stimulus again? [0 = no, 1 = yes]', 0);
             else
                 keepGoing = false;
             end
+            
         end
         
         % Show the stimulus
@@ -143,5 +137,6 @@ for ii = 1:params.NStimuli
 end
 
 % Save the data as in the end
-save(fullfile(savePath, saveFileMAT), 'data', 'params');
+save(fullfile(savePath, saveFileMAT), 'data', 'protocolParams');
 fprintf('* Data saved.\n');
+protocolParams = Psychophysics.SessionLog(protocolParams,mfilename,'StartEnd','end');

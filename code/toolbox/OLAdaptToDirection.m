f054a0e956b9c3b6c0a283ed6c097187d8401903
Joris Vincent <joris.vincent@pennmedicine.upenn.edu>
2018-05-10 13:46:34 -0400

    OLAdaptToDirection uses durations
    Use Matlab duration-type. Makes use of durationToStr, in BrainardLabToolbox.
    Also adds examples.

--- code/toolbox/OLAdaptToDirection.m
+++ code/toolbox/OLAdaptToDirection.m
@@ -1,8 +1,8 @@
-function OLAdaptToDirection(direction, oneLight, duration, varargin)
+function OLAdaptToDirection(direction, oneLight, adaptationDuration, varargin)
 % Show OLDirection as adaptation stimulus
 %
 % Syntax:
-%   OLAdaptToDirection(direction, OneLight, duration)
+%   OLAdaptToDirection(direction, OneLight, adaptationDuration)
 %
 % Description:
 %    Puts the given Direction on the OneLight, for the specified duration.
@@ -14,50 +14,93 @@ function OLAdaptToDirection(direction, oneLight, duration, varargin)
 % Inputs:
 %    direction - OLDirection object 
 %    OneLight  - OneLight object controlling a (simulated) OneLight device.
-%    duration  - numeric scalar, adaptation duration in seconds.
+%    duration  - duration, adaptation duration, e.g.: seconds(60),
+%                minutes(5)
 %
 % Outputs:
 %    None.
 %
 % Optional key/value pairs:
-%    'countdownInterval' - numeric scalar, interval (in seconds) between
-%                          verbal countdowns. Default 30s. If set to 0,
-%                          will not give any verbal feedback.
+%    'countdownInterval' - duration, interval between verbal countdowns.
+%                          Last verbal countdown is [countdownInterval]
+%                          away from 0s remaining, and every multiple from
+%                          there. Default seconds(30). If set to
+%                          seconds(0), will not give any verbal feedback.
+%
+% Notes:
+%    * With the overhead processing, especially the first verbal
+%      'Adaptation Starting', timing is imprecise on the order of 1-5
+%      seconds. For adaptation, that should be fine.
+%
+% Examples in source code
 %
 % See also:
-%    OLShowDirection
+%    OLShowDirection, duration
 
 % History:
 %    05/08/18  jv  wrote it.
+
+%{
+    oneLight = OneLight('simulate',true);
+    calibration = OLGetCalibrationStructure('CalibrationFolder',fileparts(which('OLDemoCal.mat')),'CalibrationType','DemoCal');
+    P = calibration.describe.numWavelengthBands;  % number of effective device primaries
+    primary = .5 * ones(P,1); % all primaries half-on    
+    direction = OLDirection_unipolar(primary,calibration);
+    
+    %% Adapt 1.5 minutes, with countdown every 30s
+    OLAdaptToDirection(direction,oneLight,minutes(1.5));
+%}
+%{
+    oneLight = OneLight('simulate',true);
+    calibration = OLGetCalibrationStructure('CalibrationFolder',fileparts(which('OLDemoCal.mat')),'CalibrationType','DemoCal');
+    P = calibration.describe.numWavelengthBands;  % number of effective device primaries
+    primary = .5 * ones(P,1); % all primaries half-on    
+    direction = OLDirection_unipolar(primary,calibration);
+    
+    %% Adapt 45 seconds, with countdown at 40s and 20s.
+    OLAdaptToDirection(direction,oneLight,seconds(45),'countdownInterval',seconds(20));
+%}
+%{
+    oneLight = OneLight('simulate',true);
+    calibration = OLGetCalibrationStructure('CalibrationFolder',fileparts(which('OLDemoCal.mat')),'CalibrationType','DemoCal');
+    P = calibration.describe.numWavelengthBands;  % number of effective device primaries
+    primary = .5 * ones(P,1); % all primaries half-on    
+    direction = OLDirection_unipolar(primary,calibration);
+    
+    %% Adapt 45 seconds, no countdown.
+    OLAdaptToDirection(direction,oneLight,seconds(45),'countdownInterval',seconds(0));
+%}
 
 %% Input validation
 parser = inputParser;
 parser.addRequired('direction',@(x) isa(x,'OLDirection'));
 parser.addRequired('oneLight',@(x) isa(x,'OneLight'));
-parser.addRequired('duration',@(x) validateattributes(x,{'numeric'},{'scalar','positive'}));
-parser.addParameter('countdownInterval',30,@(x) validateattributes(x,{'numeric'},{'scalar','positive'}));
-parser.parse(direction, oneLight, duration, varargin{:});
+parser.addRequired('adaptationDuration',@(x) validateattributes(x,{'duration'},{'scalar'}));
+parser.addParameter('countdownInterval',seconds(30),@(x) validateattributes(x,{'duration'},{'scalar'}));
+parser.parse(direction, oneLight, adaptationDuration, varargin{:});
 
 %% Set up countdown
-if parser.Results.countdownInterval
-    countdownTimes = parser.Results.countdownInterval:parser.Results.countdownInterval:duration;
+if parser.Results.countdownInterval > 0
+    countdownTimes = parser.Results.countdownInterval:parser.Results.countdownInterval:adaptationDuration;
     countdownTimes = fliplr(countdownTimes);
+    if countdownTimes(1) == adaptationDuration
+        countdownTimes = countdownTimes(2:end);
+    end
 else
     countdownTimes = [];
 end
 
 %% Show adaptation
 OLShowDirection(direction, oneLight);
-Speak(sprintf('Adaptation started, for %d seconds',duration),[],230);
-startTime = mglGetSecs;
-endTime = mglGetSecs + duration;
-eta = endTime-mglGetSecs;
+Speak(sprintf('Adaptation started, for %s',durationToStr(adaptationDuration)),[],230);
+endTime = mglGetSecs + seconds(adaptationDuration);
+eta = seconds(endTime-mglGetSecs);
 while eta > 0
    if ~isempty(countdownTimes) && eta < countdownTimes(1)
-       Speak(sprintf('%d seconds of adaptation remaining',countdownTimes(1)),[],230);
+       Speak(sprintf('%s of adaptation remaining',durationToStr(countdownTimes(1))),[],230);
        countdownTimes = countdownTimes(2:end);
    end
-   eta = endTime-mglGetSecs;
+   eta = seconds(endTime-mglGetSecs);
 end
 Speak('Adaptation complete.',[],230);
 

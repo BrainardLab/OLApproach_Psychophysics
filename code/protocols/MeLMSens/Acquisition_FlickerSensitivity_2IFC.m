7827e6b9fb61c75851977e46bfc36cc1625b28b5
Joris Vincent <joris.vincent@pennmedicine.upenn.edu>
2018-07-20 13:27:18 -0400

    Trial_NIFC in Acquisition_FlickerSensitivity

--- code/protocols/MeLMSens/Acquisition_FlickerSensitivity_2IFC.m
+++ code/protocols/MeLMSens/Acquisition_FlickerSensitivity_2IFC.m
@@ -18,7 +18,7 @@ classdef Acquisition_FlickerSensitivity_2IFC < handle
         % Post-validation
         validationAtThreshold;
         validatedContrastAtThresholdPos;
-        validatedContrastAtThresholdNeg;        
+        validatedContrastAtThresholdNeg;
     end
     
     %% Timing related properties
@@ -33,9 +33,9 @@ classdef Acquisition_FlickerSensitivity_2IFC < handle
         flickerFrequency = 5;
         flickerDuration = .5;
     end
-      
+    
     %% Staircase related properties
-    properties 
+    properties
         staircases;
         thresholds;
         
@@ -51,11 +51,6 @@ classdef Acquisition_FlickerSensitivity_2IFC < handle
         nUps = [3 2 1];
         nDowns = [1 1 1];
         rngSettings;
-    end
-    
-    %% Keybindings
-    properties
-        keyBindings = containers.Map();
     end
     
     %% Methods
@@ -83,12 +78,6 @@ classdef Acquisition_FlickerSensitivity_2IFC < handle
             obj.background = background;
             obj.direction = direction;
             obj.receptors = receptors;
-            
-            % Set keybindings
-            obj.keyBindings('Q') = 'abort';
-            obj.keyBindings('ESCAPE') = 'abort';
-            obj.keyBindings('GP:LOWERLEFTTRIGGER') = [1 0];
-            obj.keyBindings('GP:LOWERRIGHTTRIGGER') = [0 1];
         end
         
         function initializeStaircases(obj)
@@ -110,28 +99,29 @@ classdef Acquisition_FlickerSensitivity_2IFC < handle
             %% Show adaptation spectrum for adaptation period (preceding any trials)
             OLAdaptToDirection(obj.background, oneLight, obj.adaptationDuration);
         end
-            
-        function runAcquisition(obj, oneLight)
+        
+        function runAcquisition(obj, oneLight, responseSys)
             %% Run the acquisition
             % Create flickerWaveform;
             obj.flickerWaveform = sinewave(obj.flickerDuration,obj.samplingFq,obj.flickerFrequency);
             
             % Adapt
             Speak('Press key to start adaptation',[],230);
-            WaitForKeyChar;
+            responseSys.waitForResponse;
             obj.showAdaptation(oneLight);
+            responseSys.waitForResponse;
             
             % Run trials
             abort = false;
             for ntrial = 1:obj.NTrialsPerStaircase % loop over trial numbers
-                for k = Shuffle(1:obj.NInterleavedStaircases) % loop over staircases, in randomized order            
+                for k = Shuffle(1:obj.NInterleavedStaircases) % loop over staircases, in randomized order
                     if ~abort
-                        % Get contrast value                    
+                        % Get contrast value
                         flickerContrast = getCurrentValue(obj.staircases{k});
-
+                        
                         % Run trial
-                        [correct, abort] = obj.runTrial(flickerContrast, oneLight);
-
+                        [correct, abort] = obj.runTrial(flickerContrast, oneLight, responseSys);
+                        
                         % Update modulation parameters, according to staircase
                         obj.staircases{k} = updateForTrial(obj.staircases{k}, flickerContrast, correct);
                     end
@@ -139,55 +129,24 @@ classdef Acquisition_FlickerSensitivity_2IFC < handle
             end
             if abort
                 throw(MException('OLApproach_PsychophysicsEngine:UserAbort', ...
-                      'User aborted acquisition.'));
+                    'User aborted acquisition.'));
             end
         end
         
-        function [correct, abort] = runTrial(obj, flickerContrast, oneLight)
-            %% Assemble trial 
+        function [correct, abort] = runTrial(obj, flickerContrast, oneLight, trialResponseSys)
+            %% Assemble trial
             % Assemble modulations
             scaledDirection = obj.direction.ScaleToReceptorContrast(obj.background, obj.receptors, [flickerContrast, flickerContrast, flickerContrast, 0]');
             targetModulation = OLAssembleModulation([obj.background, scaledDirection],[ones(1,length(obj.flickerWaveform)); obj.flickerWaveform]);
             referenceModulation = OLAssembleModulation(obj.background, ones([1,length(obj.flickerWaveform)]));
-
-            % Determine which interval (1 or 2) will have flicker
-            targetPresent = logical([0 0]);
-            targetInterval = randi(length(targetPresent));
-            targetPresent(targetInterval) = true;
+            trial = Trial_NIFC(2,targetModulation,referenceModulation);
             
-            % Assemble trial
-            modulations = repmat(referenceModulation,[length(targetPresent),1]);
-            modulations(targetPresent) = targetModulation;
-            
-            %% Show modulations
+            %% Show trial
             OLShowDirection(obj.background, oneLight);
-            for m = 1:length(modulations)
-                mglWaitSecs(obj.ISI);
-                Beeper;
-                OLFlicker(oneLight, modulations(m).starts, modulations(m).stops, 1/obj.samplingFq,1);
-                OLShowDirection(obj.background, oneLight);
-            end
+            [abort, trial] = trial.run(oneLight,obj.samplingFq,trialResponseSys);
+            OLShowDirection(obj.background, oneLight);
             
-            %% Response
-            %  Get response from GamePad, but also listen to keyboard
-            while true
-                responseKey = upper(WaitForKeyChar);
-                if any(strcmp(responseKey,obj.keyBindings.keys()))
-                    break;
-                end
-            end
-            response = obj.keyBindings(responseKey);
-            
-            if ischar(response) && strcmpi(response,'abort')
-                % User wants to abort
-                abort = true;
-                correct = false;
-            else
-                abort = false;
-                
-                % Correct? Compare response
-                correct = all(response == targetPresent);
-            end
+            correct = trial.correct;
         end
     end
 end

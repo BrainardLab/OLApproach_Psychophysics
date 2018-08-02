4bd64740dbd6b17ff0cf3a7498528effcbbac3c5
Joris Vincent <joris.vincent@pennmedicine.upenn.edu>
2018-08-02 18:02:29 -0400

    Stub for merging CSVs in dir

---
+++ code/analysis/MeLMSens/lib/mergeCSVsInDir.R
@@ -0,0 +1,6 @@
+#mergeCSVinDir = function(dirpath) {
+  library(tidyverse)
+  list.files(path = dirpath, pattern ="results-.*csv") %>%
+    lapply(.,function(x) read_csv(paste0(dirpath,x))) %>%
+    bind_rows
+#}

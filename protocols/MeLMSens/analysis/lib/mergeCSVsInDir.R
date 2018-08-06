9c264ff50cab84eb6d7ef0577597ddf16765cb47
Joris Vincent <joris.vincent@gmail.com>
2018-08-05 21:58:18 -0400

    R function for merging CSVs in a directory

--- protocols/MeLMSens/analysis/lib/mergeCSVsInDir.R
+++ protocols/MeLMSens/analysis/lib/mergeCSVsInDir.R
@@ -1,6 +1,10 @@
-#mergeCSVinDir = function(dirpath) {
+mergeCSVinDir = function(dirpath) {
   library(tidyverse)
   list.files(path = dirpath, pattern ="results-.*csv") %>%
-    lapply(.,function(x) read_csv(paste0(dirpath,x))) %>%
-    bind_rows
-# }
+    lapply(.,function(x) {
+      read_csv(paste0(dirpath,'/',x)) %>%
+      add_column(filename=x,.before=1)
+      }) %>%
+    bind_rows %>%
+    return(.)
+}

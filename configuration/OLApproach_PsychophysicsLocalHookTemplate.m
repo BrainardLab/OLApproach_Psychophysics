b237656053663aff7f32b91de802b5984580ddb9
Joris Vincent <joris.vincent@pennmedicine.upenn.edu>
2018-08-08 11:58:22 -0400

    Update LocalHook, config JSON after reorganizatiion

--- configuration/OLApproach_PsychophysicsLocalHookTemplate.m
+++ configuration/OLApproach_PsychophysicsLocalHookTemplate.m
@@ -1,43 +1,84 @@
 function OLApproach_PsychophysicsLocalHook
-% Configure things for working on OneLight projects.
+% Configure paths and preferences for the OLApproach_Psychophsycis
 %
-% For use with the ToolboxToolbox.  If you copy this into your
-% ToolboxToolbox localToolboxHooks directory (by default,
+% The git repository for the OLApproach defines a directory structure that
+% is assumed by this script:
+%   OLApproach_Psychophysics
+%	??? configuration
+%	??? toolbox
+%	??? data
+%   ?   ??? raw
+%   ?   ?   ??? protocol1
+%   ?   ?   ??? protocol2
+%   ?   ?   ??? ...
+%   ?   ??? processed
+%   ?       ??? protocol1
+%   ?       ??? protocol2
+%   ?       ??? ...
+%	??? protocols
+%       ??? protocol1
+%       ?   ??? experiment
+%       ?   ??? analysis
+%       ?   ??? data
+%       ?       ??? raw
+%       ?       ??? processed
+%       ??? protocol2
+%       ??? ...
+%       ??? DefineProtocolNames.m
+%
+% OLApproach_Psychophysics - the toplevel domain for the approach. The path
+%                            to this will be stored in
+%                            pref(approach,'ApproachPath'). This is also
+%                            the path returned by
+%                            tbLocateProject('OLApproach_Psychophysics'))
+% ./configuration          - directory for any configuration scripts,
+%                            routines, etc. Contains this
+%                            LocalHookTemplate. Also contains the TbTb
+%                            .json configuration file
+% ./toolbox                - directory containing any protocol-independent
+%                            functions, scripts, utilities, etc.
+% ./data                   - directory containing two symbolic links
+% ./data/raw               - symlink to the data directory where ALL raw 
+%                            data for this approach will be stored.
+%                            In the Aguirre-Brainard labs, this should
+%                            point to [Dropbox]/MELA_data/Experiments/OLApproach_Psychophysics/
+% ./data/processed         - symlink to the data directory where ALL
+%                            processed data for this approach will be
+%                            stored. In the Aguirre-Brainard labs, this
+%                            should point to [Dropbox]/MELA_analysis/experiments/OLApproach_Psychophysics/
+% ./protocols              - directory containing a subdirectory for each
+%                            specific protocol. The ./protocols directory
+%                            also contains the script
+%                            DefineProtocolNames.m, which this local hook
+%                            uses.
+% ./protocols/protocol1/   - subdirectory for protocol 1. All files
+%                            relating to that protocol should be kept here.
+% .../protocol1/experiment - code/files for running the experiment, i.e.,
+%                            producing the raw data.
+% .../protocol1/analysis   - code/files for analyzing the data
+% .../data/raw             - symlink to ./data/raw/protocol1
+% .../data/processed       - synlink to ./data/processed/protocol1
+%
+% The protocol-specific symlinks Protocol1/data/raw and
+% Protocol1/data/processed get automatically created by this script, and
+% overridden if they exist.
+%
+% This local hook is for use with the ToolboxToolbox.  If you copy this
+% into your ToolboxToolbox localToolboxHooks directory (by default,
 % ~/localToolboxHooks) and delete "LocalHooksTemplate" from the filename,
 % this will get run when you execute
 %   tbUseProject('OLApproach_Psychophysics')
-% to set up for this project.  You then edit your local copy to match your local machine.
-%
-% The main thing that this does is define Matlab preferences that specify input and output
-% directories.
-%
-% You will need to edit the project location and i/o directory locations
-% to match what is true on your computer.
+% to set up for this project.  You then edit your local copy to match your
+% local machine.
 
 %% Say hello
 fprintf('Running OLApproach_Psychophysics local hook\n');
 approach = 'OLApproach_Psychophysics';
+approachPath = fullfile(tbLocateProject(approach));
 
-%% Define Dropbox path
+%% Define Dropbox paths
 dropboxPath = fullfile('~','Dropbox (Aguirre-Brainard Lab)');
 
-%% Define protocols for this approach
-protocols = DefineProtocolNames;
-
-%% Remove old preferences
-if (ispref(approach))
-    rmpref(approach);
-end
-for pp = 1:length(protocols)
-    if (ispref(protocols{pp}))
-        rmpref(protocols{pp});
-    end
-end
-
-%% Set pref to point at the code for this approach
-setpref(approach,'CodePath', fullfile(tbLocateProject(approach),'code'));
-
-%% Set Dropbox-related paths
 % Define the following paths:
 %  - materials:	[dropbox]/MELA_materials/Experiments/[Approach]
 %  - data:      [dropbox]/MELA_data/Experiments/[Approach]
@@ -54,7 +95,55 @@ assert(isdir(dataBasePath),'Data basepath (%s) does not exist', dataBasePath);
 assert(isdir(calBasePath),'Calibration basepath (%s) does not exist', calBasePath);
 assert(isdir(analysisBasePath),'Analysis basepath (%s) does not exist',analysisBasePath);
 
-% Set as perferences
+%% Create data symlinks
+rawDataLinkCommand = sprintf('ln -s %s %s',...
+	replace(dataBasePath,{'(',')',' '},{'\(','\)','\ '}),...
+    fullfile(approachPath,'data','raw'));
+system(rawDataLinkCommand);
+
+procDataLinkCommand = sprintf('ln -s %s %s',...
+	replace(dataBasePath,{'(',')',' '},{'\(','\)','\ '}),...
+    fullfile(approachPath,'data','processed'));
+system(procDataLinkCommand);
+
+%% Set Directory structures for individual protocols
+protocols = DefineProtocolNames;
+for pp = 1:length(protocols)
+    protocolDir = fullfile(approachPath,'protocols',protocols{pp});
+    mkdir(protocolDir,'data');
+    
+    rawDataDestination = fullfile(protocolDir,'data','raw');
+    if ~unix(['test -L ',rawDataDestination])
+        delete(rawDataDestination)
+    end
+    rawDataLinkCommand = sprintf('ln -s %s %s',...
+        fullfile(approachPath,'data','raw',protocols{pp}),...
+        rawDataDestination);
+    system(rawDataLinkCommand);
+    
+    processedDataDestination = fullfile(protocolDir,'data','processed');
+    if ~unix(['test -L ',processedDataDestination])
+        delete(processedDataDestination)
+    end
+    processedDataLinkCommand = sprintf('ln -s %s %s',...
+        fullfile(approachPath,'data','processed',protocols{pp}),...
+        processedDataDestination);
+    system(processedDataLinkCommand);
+end
+
+%% Set MATLAB preferences
+% Remove old preferences
+if (ispref(approach))
+    rmpref(approach);
+end
+for pp = 1:length(protocols)
+    if (ispref(protocols{pp}))
+        rmpref(protocols{pp});
+    end
+end
+
+% Set path preferences
+setpref(approach,'ApproachPath', approachPath);
 setpref(approach, 'MaterialsPath', materialsBasePath);
 setpref(approach, 'DataPath', dataBasePath); 
 setpref(approach, 'OneLightCalDataPath', calBasePath);
@@ -62,54 +151,6 @@ setpref(approach, 'AnalysisPath',analysisBasePath);
 
 % Overwrite OneLightToolbox preference for calibrations
 setpref('OneLightToolbox','OneLightCalData',getpref(approach,'OneLightCalDataPath'));
-
-%% Set Directory structures for individual protocols
-for pp = 1:length(protocols)
-    % Set prefs
-    setpref(protocols{pp},'DataFilesBasePath',fullfile(dataBasePath,protocols{pp}));
-    setpref(protocols{pp},'AnalysisBasePath',fullfile(analysisBasePath,protocols{pp}));
-    
-    % Create symbolic links to dropbox folders, in the
-    % `analysis/[protocol]` directory. The symbolic link is a subdirectory 
-    % in `analysis/[protocol]` that points directly to the dropbox 
-    % directory. This makes filepath specifications in data analysis a lot
-    % easier. The following links are created:
-    %  - analysis/[protocol]/data/raw: MELA_data/Experiments/[Approach]/[protocol]
-    %  - analysis/[protocol]/data/processed: MELA_analysis/Experiments/[Approach]/[protocol]
-    protocolAnalysisDir = fullfile(getpref(approach,'CodePath'),'analysis',protocols{pp});
-    mkdir(protocolAnalysisDir,'data');
-    
-    rawDataDestination = fullfile(protocolAnalysisDir,'data','raw');
-    if ~unix(['test -L ',rawDataDestination])
-        delete(rawDataDestination)
-    end
-    rawDataLinkCommand = sprintf('ln -s %s %s',...
-        replace(getpref(protocols{pp},'DataFilesBasePath'),{'(',')',' '},{'\(','\)','\ '}),...
-        rawDataDestination);
-    system(rawDataLinkCommand);
-    
-    processedDataDestination = fullfile(protocolAnalysisDir,'data','processed');
-    if ~unix(['test -L ',processedDataDestination])
-        delete(processedDataDestination)
-    end
-    processedDataLinkCommand = sprintf('ln -s %s %s',...
-        replace(getpref(protocols{pp},'AnalysisBasePath'),{'(',')',' '},{'\(','\)','\ '}),...
-        processedDataDestination);
-    system(processedDataLinkCommand);
-    
-    % Add the symlinks to .gitignore
-    % Since on different machines, these links might not work / need to
-    % point to a different directory, the links should NOT be under source
-    % control. We add them to .gitignore to take care of that.
-    gitIgnoreFID = fopen(fullfile(getpref(approach,'CodePath'),'..','.gitignore'),'a+');
-    frewind(gitIgnoreFID);
-    gitIgnore = textscan(gitIgnoreFID,'%s','Delimiter','\n');
-    ignoreLine = fullfile('/','code','analysis',protocols{pp},'data');
-    if isempty(gitIgnore) || ~any(contains(string(gitIgnore{:}),ignoreLine))
-        fprintf(gitIgnoreFID,[ignoreLine '\n']);
-    end
-    fclose(gitIgnoreFID);
-end
 
 %% Set simulate.
 simulate.oneLight = true;
@@ -123,3 +164,16 @@ setpref(approach, 'SpeakRateDefault', 230);
 %% Add OmniDriver.jar to java path
 OneLightDriverPath = tbLocateToolbox('OneLightDriver');
 JavaAddToPath(fullfile(OneLightDriverPath,'xOceanOpticsJava/OmniDriver.jar'),'OmniDriver.jar');
+
+%% Add the symlinks to .gitignore
+%     % Since on different machines, these links might not work / need to
+%     % point to a different directory, the links should NOT be under source
+%     % control. We add them to .gitignore to take care of that.
+%     gitIgnoreFID = fopen(fullfile(getpref(approach,'CodePath'),'..','.gitignore'),'a+');
+%     frewind(gitIgnoreFID);
+%     gitIgnore = textscan(gitIgnoreFID,'%s','Delimiter','\n');
+%     ignoreLine = fullfile('/','code','analysis',protocols{pp},'data');
+%     if isempty(gitIgnore) || ~any(contains(string(gitIgnore{:}),ignoreLine))
+%         fprintf(gitIgnoreFID,[ignoreLine '\n']);
+%     end
+%     fclose(gitIgnoreFID);

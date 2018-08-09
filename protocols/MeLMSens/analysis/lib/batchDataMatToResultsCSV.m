b896edc4526a51cc58ef9ae046080189ab1c64ee
Joris Vincent <joris.vincent@pennmedicine.upenn.edu>
2018-08-09 11:52:45 -0400

    replace ‘data-‘ with ‘results_acquisition-‘

--- protocols/MeLMSens/analysis/lib/batchDataMatToResultsCSV.m
+++ protocols/MeLMSens/analysis/lib/batchDataMatToResultsCSV.m
@@ -4,5 +4,6 @@ for m = 1:numel(matfiles)
     inputFile = fullfile(dirEntry.folder,dirEntry.name);
     outputFile = replace(inputFile,'raw','processed');
     outputFile = replace(outputFile,'.mat','.csv');
+    outputFile = replace(outputFile,'data-','results_acquisition-');
     dataMatToResultsCSV(inputFile,outputFile);
 end

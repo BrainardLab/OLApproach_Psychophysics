8d2456fe2fd492c3b561e79a169eb0a010806c22
Joris Vincent <joris.vincent@gmail.com>
2018-08-23 15:39:21 -0400

    Express results in excitaton / JND

--- protocols/MeLMSens/analysis/lib/dataMatToResultsCSV.m
+++ protocols/MeLMSens/analysis/lib/dataMatToResultsCSV.m
@@ -5,12 +5,19 @@ load(inputFilePath);
 %% Extract results from raw datafile
 for i = 1:numel(acquisition)
     acquisitionResults(i).condition = acquisition(i).name;
-    acquisitionResults(i).contrast = extractResultsFromAcquisition(acquisition(i))';
+    [contrast, excitationPos, excitationNeg, excitationBackground] = extractResultsFromAcquisition(acquisition(i));
+    acquisitionResults(i).contrast = contrast';
+    acquisitionResults(i).excitationPos = excitationPos';
+    acquisitionResults(i).excitationNeg = excitationNeg';
+    acquisitionResults(i).excitationBackground = excitationBackground';
 end
 
 %% Convert to table
 acquisitionResults = struct2table(acquisitionResults);
 acquisitionResults = splitvars(acquisitionResults,'contrast','NewVariableNames',{'contrast_L','contrasts_M','contrast_S','contrast_Mel'});
+acquisitionResults = splitvars(acquisitionResults,'excitationPos','NewVariableNames',{'excitationPos_L','excitationPos_M','excitationPos_S','excitationPos_Mel'});
+acquisitionResults = splitvars(acquisitionResults,'excitationNeg','NewVariableNames',{'excitationNeg_L','excitationNeg_M','excitationNeg_S','excitationNeg_Mel'});
+acquisitionResults = splitvars(acquisitionResults,'excitationBackground','NewVariableNames',{'excitationBackground_L','excitationBackground_M','excitationBackground_S','excitationBackground_Mel'});
 
 %% Save
 outputFileDir = fileparts(outputFilePath);
--- protocols/MeLMSens/analysis/lib/extractResultsFromAcquisition.m
+++ protocols/MeLMSens/analysis/lib/extractResultsFromAcquisition.m
@@ -1,17 +1,23 @@
-function contrast = extractResultsFromAcquisition(acquisition)
+function [contrast, excitationPos, excitationNeg, excitationBackground] = extractResultsFromAcquisition(acquisition)
     validations = acquisition.validationAtThreshold;
 
     contrastActual = [];
+    excitationActual = [];
     for v = validations
         contrastActual = cat(3,contrastActual,v.contrastActual(:,[1,3]));
+        excitationActual = cat(3,excitationActual,v.excitationActual(:,1:3));
     end
 
     % Average over validations
-    medianContrastActual = median(contrastActual,3);
-
-    % Average over positive and negative excursion
-    medianContrastActual = median(abs(medianContrastActual),2);
+    meanContrastActual = mean(contrastActual,3);
+    meanExcitation = mean(excitationActual,3);
     
+    % Average over positive and negative excursion
+    meanContrastActual = mean(abs(meanContrastActual),2);  
+        
     % Return
-    contrast = medianContrastActual;
+    contrast = meanContrastActual;
+    excitationPos = meanExcitation(:,2);
+    excitationNeg = meanExcitation(:,3);  
+    excitationBackground = meanExcitation(:,1);
 end

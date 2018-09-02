6d91306c4e6b3c5fdbe48152b510c3d96ab1e80a
Joris Vincent <joris.vincent@gmail.com>
2018-09-01 22:17:25 -0400

    projectorSpot class
    turn projectorSpot functions into a `projectorSpot` class

---
+++ toolbox/@projectorSpot/projectorSpot.m
@@ -0,0 +1,190 @@
+classdef projectorSpot < handle
+    %PROJECTORSPOT Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        projectorWindow;
+        isOn = false;
+    end
+    
+    methods
+        function obj = projectorSpot(varargin)           
+            %% make projectorWindow
+            obj.makeProjectorWindow(varargin{:});
+            
+            %% Add spot
+            obj.addSpot;
+        end
+        
+        function projectorWindow = makeProjectorWindow(obj, varargin)
+            % Create GLWindow to control display
+            %
+            % Syntax:
+            %   projectorWindow = makeProjectorWindow;
+            %
+            % Description:
+            %
+            %
+            % Inputs:
+            %    None.
+            % 
+            % Outputs:
+            %    projectorWindow - GLWindow, open on last display
+            %
+            % Optional key/value arguments:
+            %    None.
+            %
+            % See also:
+            %    projectorSpot, projectorSpot.makeSpot, projectorSpot.open
+            %    projectorSpot.close
+
+            % History:
+            %    07/16/18  jv  wrote makeProjectorSpot.
+            %    09/01/18  jv  turn into projectorSpot.makeProjectorWindow
+            %                  method.                
+            displayInfo = mglDescribeDisplays;
+            lastDisplay = length(displayInfo);
+            
+            %% Parse input
+            parser = inputParser;
+            parser.addParameter('WindowID',lastDisplay,@isnumeric);
+            parser.addParameter('Fullscreen',false,@islogical);
+            parser.addParameter('BackgroundColor',[0 0 0],@isnumeric);
+            parser.addParameter('SceneDimensions',displayInfo(lastDisplay).screenSizePixel);
+            parser.addParameter('WindowPosition',[0 0],@isnumeric);
+            parser.addParameter('WindowSize',[300 300],@isnumeric);
+            parser.parse(varargin{:});
+            
+            %% Create a GLWindow object
+            screenSizeInPixels = displayInfo(lastDisplay).screenSizePixel;
+            projectorWindow = GLWindow('SceneDimensions', screenSizeInPixels, ...
+                'BackgroundColor', parser.Results.BackgroundColor,...
+                'WindowID',        parser.Results.WindowID,...
+                'Fullscreen',      parser.Results.Fullscreen,...
+                'WindowPosition',  parser.Results.WindowPosition,...
+                'WindowSize',      parser.Results.WindowSize);   
+            obj.projectorWindow = projectorWindow;
+        end
+        
+        function open(obj)
+            try
+                obj.projectorWindow.open();
+            catch E
+                obj.projectorWindow.close();
+                rethrow(E);
+            end
+        end
+        
+        function close(obj)
+            obj.isOn = false;
+            obj.projectorWindow.close();
+        end
+        
+        function delete(obj)
+            obj.close();
+        end
+        
+        function show(obj)
+            % Toggles a projector spot on
+            %
+            % Syntax:
+            %   spot.show
+            %   show(spot)
+            %
+            % Description:
+            %    Detailed explanation goes here
+            %
+            % Inputs:
+            %    obj  - projectorSpot object
+            %
+            % Outputs:
+            %    None. - Objects defining projector spot have now been
+            %            enabled.
+            %
+            % Optional key/value arguments:
+            %    None.
+            %
+            % See also:
+            %    projectorSpot, projectorSpot.hide, projectorSpot.toggle, 
+            %    GLWindow
+
+            % History:
+            %    07/17/18  jv   wrote toggleProjectorSpot.
+            %    09/01/18  jv   turn into projectorSpot.show method.        
+            obj.open();
+            obj.isOn = true;
+            obj.projectorWindow.enableAllObjects();
+            obj.projectorWindow.draw();
+        end
+        
+        function hide(obj)
+            % Toggles a projector spot on
+            %
+            % Syntax:
+            %   spot.hide
+            %   hide(spot)
+            %
+            % Description:
+            %    Detailed explanation goes here
+            %
+            % Inputs:
+            %    obj  - projectorSpot object
+            %
+            % Outputs:
+            %    None. - Objects defining projector spot have now been
+            %            disabled.
+            %
+            % Optional key/value arguments:
+            %    None.
+            %
+            % See also:
+            %    projectorSpot, projectorSpot.show, projectorSpot.toggle, 
+            %    GLWindow
+
+            % History:
+            %    07/17/18  jv   wrote toggleProjectorSpot.
+            %    09/01/18  jv   turn into projectorSpot.hide method.               
+            obj.open();
+            obj.isOn = false;
+            obj.projectorWindow.disableAllObjects();
+            obj.projectorWindow.draw();
+        end
+        
+        function isOn = toggle(obj)
+            % Toggles a projector spot on or off
+            %
+            % Syntax:
+            %   spot.toggle();
+            %   isOn = spot.toggle();
+            %
+            % Description:
+            %    Detailed explanation goes here
+            %
+            % Inputs:
+            %    obj  - projectorSpot object
+            %
+            % Outputs:
+            %    isOn - scalar boolean, projectorSpot.isOn property after
+            %           toggle
+            %
+            % Optional key/value arguments:
+            %    None.
+            %
+            % See also:
+            %    projectorSpot, projectorSpot.show, projectorSpot.hide, 
+            %    GLWindow
+
+            % History:
+            %    07/17/18  jv   wrote toggleProjectorSpot.
+            %    09/01/18  jv   turn into projectorSpot.toggle method.
+            %                   Remove 'toggleOn' arg, deprecated by
+            %                   show/hide methods.
+            if ~obj.isOn
+                obj.show();
+            else
+                obj.hide();
+            end
+            isOn = obj.isOn;
+        end
+    end
+end

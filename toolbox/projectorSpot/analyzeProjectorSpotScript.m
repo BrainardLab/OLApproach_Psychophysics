e2c83d25243e4807c09318b2d18f0b38b12307c1
Joris Vincent <joris.vincent@gmail.com>
2018-09-03 21:28:28 -0400

    Update projector measurements analyze script

--- toolbox/projectorSpot/analyzeProjectorSpotScript.m
+++ toolbox/projectorSpot/analyzeProjectorSpotScript.m
@@ -1,53 +1,14 @@
-plotAll(SPDs);
+lums = SPDsRawToLumsRaw(SPDs,[380 2 201]);
+plotSPDsRawAll(SPDs);
+plotLumsRawAll(lums);
+plotRawAll(SPDs,lums);
 
-%% Support functions
-function lums = SPDsLocationToLums(SPDs,S)
-lums = cellfun(@(x) SPDToLum(x,S),SPDs);
-% lums = SPDToLum(cell2mat(SPDs(:)'),S);
-% lums = [lums([1,3]); lums([2, 4])];
-end
-
-
-function plotLumsForLocation(lums, ax)
-parser = inputParser;
-parser.addRequired('SPDs',@iscell);
-axes(ax); hold on;
-bar(lums);
-xticks([1 2]);
-xticklabels({'on', 'off'});
-%legend('mirrors on','mirrors off');
-end
-
-function F = plotAll(measurements, varargin)
-    F = figure();
-    for i = 1:2
-        for j = 1:2
-            idxs = j*4+(i-1)*8+[-1 0];
-            M = measurements{i,j};
-            plotSPDsForLocation(M,subplot(3,6,idxs(1)));
-            plotLumsForLocation(SPDsLocationToLums(M,[380 2 201]),subplot(3,6,idxs(2)))
-        end
-    end
-end
+lumsTable = lumsToTable(lums);
+projectorLumsTable = lumsToProjectorLum(lumsTable);
+projectorLumsMeans = projectorLumsMeans(projectorLumsTable);
+projectorLumMean = mean(projectorLumsMeans.mean_projectorLum);
 
 %% Average
-function deltaProjectorSPDs = calcDeltaProjectorSPDs(SPDs)
-    for i = 1:2
-        for j = 1:2
-            deltaProjectorSPDs{i,j} = [SPDs{i,j}{1,1}-SPDs{i,j}{2,1}, ...
-                                       SPDs{i,j}{1,2}-SPDs{i,j}{2,2}];
-        end
-    end
-end
-
-function avgDeltaProjectorSPDs = averagoOverOLOnOff(deltaProjectorSPDs)
-    for i = 1:2
-        for j = 1:2
-            avgDeltaProjectorSPDs{i,j} = mean(deltaProjectorSPDs,2);
-        end
-    end
-end
-
 % - Subtract projector on - off, to get projector SPD, per location per condition
 % - Average SPD across OneLight on/off conditions, per location
 %   - SEM, CI (95% CI = +- 1.96 SEM)

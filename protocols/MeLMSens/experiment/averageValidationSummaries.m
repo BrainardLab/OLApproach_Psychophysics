bdf5839defe6dc5a59a319d46f7ada4ba896f07a
Joris Vincent <joris.vincent@gmail.com>
2018-09-06 18:03:14 -0400

    Stub averageValidationSummaries

---
+++ protocols/MeLMSens/experiment/averageValidationSummaries.m
@@ -0,0 +1,24 @@
+function resultsLuminancesBg = averageValidationSummaries(luminancesBg, contrastsBg, contrastsFlicker)
+%% Average background luminances
+[G,direction] = findgroups(luminancesBg.direction);
+[median, ~,~,~,CI] = splitapply(@multiStats,luminancesBg.lumActual,G);
+desired = splitapply(@unique,luminancesBg.lumDesired,G);
+resultsLuminancesBg = table(direction,desired,median,CI);
+resultsLuminancesBg = [resultsLuminancesBg rowfun(@(x,y) x >= y(1) && x <= y(2),resultsLuminancesBg,...
+                    'InputVariables',{'desired','CI'},...
+                    'OutputVariableNames','inCI')];
+
+%% Average background contrasts
+
+
+%% Average flicker contrasts
+
+end
+
+function [med, stdSample, semean, semedian, CImedian] = multiStats(x)
+    med = median(x);
+    stdSample = std(x);
+    semean = stdSample/sqrt(length(x));
+    semedian = 1.2533 * semean;
+    CImedian = ([-1.96, +1.96] * semedian) + med;
+end

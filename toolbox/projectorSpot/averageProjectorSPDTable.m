81b5986b9eba359f37e54c617f409180ffa4a503
Joris Vincent <joris.vincent@pennmedicine.upenn.edu>
2018-09-08 15:56:01 -0400

    Bugfix: R2017a does not have `movevars` function

--- toolbox/projectorSpot/averageProjectorSPDTable.m
+++ toolbox/projectorSpot/averageProjectorSPDTable.m
@@ -5,7 +5,7 @@ function avgProjectorSPDTable = averageProjectorSPDTable(projectorSPDTable)
     lum = splitapply(@(x) [mean(x), ([-1.96, +1.96] .* std(x)/sqrt(length(x)))+mean(x)],...
                     projectorSPDTable.deltaLum,G);
     lum = table(lum(:,1), lum(:,2:3),'VariableNames',{'LumMean','LumCI'});
-    lum.location = TID; lum = movevars(lum,'location','Before',1);
+    lum.location = TID; %lum = movevars(lum,'location','Before',1);
     SPDmean = splitapply(@mean,projectorSPDTable.deltaSPD,G); 
     avgProjectorSPDTable = lum;
     avgProjectorSPDTable.SPD = SPDmean;

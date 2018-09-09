793c9cb8c78c4be27cc20745f7dbe3fd06ec2b96
Joris Vincent <joris.vincent@pennmedicine.upenn.edu>
2018-09-09 18:28:20 -0400

    Change projector spot sizes
    Would prefer to have a method for this.

--- protocols/MeLMSens/experiment/projectorSpotMeLMSens_SteadyAdapt.m
+++ protocols/MeLMSens/experiment/projectorSpotMeLMSens_SteadyAdapt.m
@@ -2,6 +2,7 @@ function pSpot = projectorSpotMeLMSens_SteadyAdapt(simulate)
 %PROJECTORSPOTMELMSENS_STEADYADAPT Summary of this function goes here
 %   Detailed explanation goes here
     pSpot = projectorSpot('fullScreen',~simulate);
-    pSpot.addSpot('spotDiameter',160,...
-                  'annulusDiameter',530);
+    pSpot.addSpot('spotDiameter',110,...
+                  'annulusDiameter',500);
+    pSpot.show();
 end

eb36d1a85fdafc4da8d19cf7999ef118f6c9bf10
Joris Vincent <joris.vincent@gmail.com>
2018-10-01 17:10:53 -0400

    Round desired contrasts to two decimals ( 0.01%)

--- protocols/MeLMSens/experiment/bipolarValidationTallTable.m
+++ protocols/MeLMSens/experiment/bipolarValidationTallTable.m
@@ -5,7 +5,7 @@ receptorStrings = {'L','M','S','Mel'}';
 time = repmat(validation.time(1),[length(receptorStrings),1]);
 
 contrastDesired = validation.contrastDesired(:,[1 3]); % desired modulation, not differential, contrast
-contrastDesired = table(time, receptorStrings,contrastDesired*100,...
+contrastDesired = table(time, receptorStrings,round(contrastDesired*100,2),...
                     'VariableNames',{'time','receptor','desired'}); % convert to table
 
 contrastActual = validation.contrastActual(:,[1 3]); % measured modulation, not differential, contrast

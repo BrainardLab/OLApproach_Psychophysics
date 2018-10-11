5368bac92db18b22a0ddb93c1c1096133a810539
Joris Vincent <joris.vincent@gmail.com>
2018-10-11 15:09:00 -0400

    Integrate OLCalibrationAddSPDToDarkLight
    OLToolbox should be responsible for how to update a calibration

--- toolbox/+projectorSpot/UpdateOLCalibrationWithProjectorSpot.m
+++ toolbox/+projectorSpot/UpdateOLCalibrationWithProjectorSpot.m
@@ -2,5 +2,5 @@ function [calibration, projSPD, projLum, SPDs] = UpdateOLCalibrationWithProjecto
 %UPDATECALIBRATIONWITHPROJECTORSPD Summary of this function goes here
 %   Detailed explanation goes here
     [projSPD, projLum, SPDs] = projectorSpot.getSPD(pSpot,oneLight, radiometer);
-    calibration = projectorSpot.UpdateOLCalibrationWithProjectorSPD(calibration,projSPD);
-end 
+    calibration = OLCalibrationAddSPDToDarkLight(calibration,projSPD);
+end

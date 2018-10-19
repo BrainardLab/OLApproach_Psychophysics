4fbb2ad2182c007963325bb20ebde1af59707f96
Joris Vincent <joris.vincent@gmail.com>
2018-10-19 12:02:10 -0400

    Make some things protected
    GLWindow related prop, methods, are not part of public interface.

--- toolbox/+projectorSpot/@projectorSpot/projectorSpot.m
+++ toolbox/+projectorSpot/@projectorSpot/projectorSpot.m
@@ -3,7 +3,6 @@ classdef projectorSpot < handle
     %   Detailed explanation goes here
     
     properties (SetAccess = protected)
-        projectorWindow;
         isOn = false;
         fullScreen = false;
         
@@ -19,6 +18,8 @@ classdef projectorSpot < handle
         centerPosition = [0 0];
     end
     properties (Access = protected)
+        projectorWindow;        
+        isOpen = false;
         children = containers.Map();
     end
 
@@ -31,6 +32,124 @@ classdef projectorSpot < handle
             obj.addSpot;
         end
         
+        function delete(obj)
+            obj.close();
+        end
+        
+        function show(obj)
+            % Toggles a projector spot on
+            %
+            % Syntax:
+            %   spot.show
+            %   show(spot)
+            %
+            % Description:
+            %    Detailed explanation goes here
+            %
+            % Inputs:
+            %    obj  - projectorSpot object
+            %
+            % Outputs:
+            %    None. - Objects defining projector spot have now been
+            %            enabled.
+            %
+            % Optional key/value arguments:
+            %    None.
+            %
+            % See also:
+            %    projectorSpot, projectorSpot.hide, projectorSpot.toggle, 
+            %    GLWindow
+
+            % History:
+            %    07/17/18  jv   wrote toggleProjectorSpot.
+            %    09/01/18  jv   turn into projectorSpot.show method.  
+            if ~obj.isOpen
+                obj.open();
+                obj.addSpot();
+            end
+            obj.projectorWindow.enableAllObjects();
+            obj.projectorWindow.draw();
+            obj.isOn = true;
+        end
+        
+        function hide(obj)
+            % Toggles a projector spot on
+            %
+            % Syntax:
+            %   spot.hide
+            %   hide(spot)
+            %
+            % Description:
+            %    Detailed explanation goes here
+            %
+            % Inputs:
+            %    obj  - projectorSpot object
+            %
+            % Outputs:
+            %    None. - Objects defining projector spot have now been
+            %            disabled.
+            %
+            % Optional key/value arguments:
+            %    None.
+            %
+            % See also:
+            %    projectorSpot, projectorSpot.show, projectorSpot.toggle, 
+            %    GLWindow
+
+            % History:
+            %    07/17/18  jv   wrote toggleProjectorSpot.
+            %    09/01/18  jv   turn into projectorSpot.hide method.               
+            obj.open();
+            obj.isOn = false;
+            obj.projectorWindow.disableAllObjects();
+            obj.projectorWindow.draw();
+        end
+        
+        function close(obj)
+            obj.projectorWindow.close();
+            obj.isOn = false;            
+            obj.isOpen = false;
+        end              
+        
+        function isOn = toggle(obj)
+            % Toggles a projector spot on or off
+            %
+            % Syntax:
+            %   spot.toggle();
+            %   isOn = spot.toggle();
+            %
+            % Description:
+            %    Detailed explanation goes here
+            %
+            % Inputs:
+            %    obj  - projectorSpot object
+            %
+            % Outputs:
+            %    isOn - scalar boolean, projectorSpot.isOn property after
+            %           toggle
+            %
+            % Optional key/value arguments:
+            %    None.
+            %
+            % See also:
+            %    projectorSpot, projectorSpot.show, projectorSpot.hide, 
+            %    GLWindow
+
+            % History:
+            %    07/17/18  jv   wrote toggleProjectorSpot.
+            %    09/01/18  jv   turn into projectorSpot.toggle method.
+            %                   Remove 'toggleOn' arg, deprecated by
+            %                   show/hide methods.
+            if ~obj.isOn
+                obj.show();
+            else
+                obj.hide();
+            end
+            isOn = obj.isOn;
+        end
+    end
+    
+    methods (Access = protected)       
         function projectorWindow = makeProjectorWindow(obj, varargin)
             % Create GLWindow to control display
             %
@@ -80,7 +199,7 @@ classdef projectorSpot < handle
                 'WindowSize',      parser.Results.WindowSize);   
             obj.projectorWindow = projectorWindow;
             obj.fullScreen = parser.Results.fullscreen;
-        end
+        end  
         
         function open(obj)
             %    09/03/18  jv   spoofFullScreen before opening. Workaround
@@ -92,118 +211,7 @@ classdef projectorSpot < handle
                 obj.projectorWindow.close();
                 rethrow(E);
             end
-        end
-        
-        function close(obj)
-            obj.isOn = false;
-            obj.projectorWindow.close();
-        end
-        
-        function delete(obj)
-            obj.close();
-        end
-        
-        function show(obj)
-            % Toggles a projector spot on
-            %
-            % Syntax:
-            %   spot.show
-            %   show(spot)
-            %
-            % Description:
-            %    Detailed explanation goes here
-            %
-            % Inputs:
-            %    obj  - projectorSpot object
-            %
-            % Outputs:
-            %    None. - Objects defining projector spot have now been
-            %            enabled.
-            %
-            % Optional key/value arguments:
-            %    None.
-            %
-            % See also:
-            %    projectorSpot, projectorSpot.hide, projectorSpot.toggle, 
-            %    GLWindow
-
-            % History:
-            %    07/17/18  jv   wrote toggleProjectorSpot.
-            %    09/01/18  jv   turn into projectorSpot.show method.  
-            obj.open();
-            obj.isOn = true;
-            obj.projectorWindow.enableAllObjects();
-            obj.projectorWindow.draw();
-        end
-        
-        function hide(obj)
-            % Toggles a projector spot on
-            %
-            % Syntax:
-            %   spot.hide
-            %   hide(spot)
-            %
-            % Description:
-            %    Detailed explanation goes here
-            %
-            % Inputs:
-            %    obj  - projectorSpot object
-            %
-            % Outputs:
-            %    None. - Objects defining projector spot have now been
-            %            disabled.
-            %
-            % Optional key/value arguments:
-            %    None.
-            %
-            % See also:
-            %    projectorSpot, projectorSpot.show, projectorSpot.toggle, 
-            %    GLWindow
-
-            % History:
-            %    07/17/18  jv   wrote toggleProjectorSpot.
-            %    09/01/18  jv   turn into projectorSpot.hide method.               
-            obj.open();
-            obj.isOn = false;
-            obj.projectorWindow.disableAllObjects();
-            obj.projectorWindow.draw();
-        end
-        
-        function isOn = toggle(obj)
-            % Toggles a projector spot on or off
-            %
-            % Syntax:
-            %   spot.toggle();
-            %   isOn = spot.toggle();
-            %
-            % Description:
-            %    Detailed explanation goes here
-            %
-            % Inputs:
-            %    obj  - projectorSpot object
-            %
-            % Outputs:
-            %    isOn - scalar boolean, projectorSpot.isOn property after
-            %           toggle
-            %
-            % Optional key/value arguments:
-            %    None.
-            %
-            % See also:
-            %    projectorSpot, projectorSpot.show, projectorSpot.hide, 
-            %    GLWindow
-
-            % History:
-            %    07/17/18  jv   wrote toggleProjectorSpot.
-            %    09/01/18  jv   turn into projectorSpot.toggle method.
-            %                   Remove 'toggleOn' arg, deprecated by
-            %                   show/hide methods.
-            if ~obj.isOn
-                obj.show();
-            else
-                obj.hide();
-            end
-            isOn = obj.isOn;
-        end
+            obj.isOpen = true;
+        end        
     end
 end

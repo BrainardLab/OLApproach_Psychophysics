d827cf429668f2ccd7f4a072bebc9d4614856895
Joris Vincent <joris.vincent@gmail.com>
2018-10-23 15:37:10 -0400

    adjust-method for projectorSpot class
    and test-function.

---
+++ toolbox/+projectorSpot/@projectorSpot/adjust.m
@@ -0,0 +1,37 @@
+function adjust(obj,inputHandler)
+%ADJUST Summary of this function goes here
+%   Detailed explanation goes here
+
+targetName = 'spot';
+
+while true %% Main loop
+    action = inputHandler.waitForResponse();
+    switch action
+        case "escape"
+            return
+        case "switch"
+            if strcmp(targetName,'spot')
+                targetName = 'annulus';
+            else
+                targetName = 'spot';
+            end
+            translation = [0 0];
+        case "left"
+            translation = [-1 0];
+        case "right"
+            translation = [+1 0];
+        case "up"
+            translation = [0 +1];
+        case "down"
+            translation = [0 -1];            
+    end
+    
+    switch targetName
+        case 'spot'
+            obj.spotCenter = obj.spotCenter + translation;
+        case 'annulus'
+            obj.annulusCenter = obj.annulusCenter + translation;
+    end
+end
+
+end

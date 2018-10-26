6edb673e656b02ecdbf4ebce27af3572cb5a4a13
Joris Vincent <joris.vincent@gmail.com>
2018-10-26 17:11:07 -0400

    Better extraction of results from acquisition

--- protocols/MeLMSens/analysis/lib/dataMatToResultsCSV.m
+++ protocols/MeLMSens/analysis/lib/dataMatToResultsCSV.m
@@ -1,27 +1,16 @@
 function dataMatToResultsCSV(inputFilePath, outputFilePath)
 %% Load raw datafile
-load(inputFilePath);
+load(inputFilePath, 'acquisition');
 
 %% Extract results from raw datafile
+acquisitionResults = table();
 for i = 1:numel(acquisition)
-    acquisitionResults(i).condition = acquisition(i).name;
-    [contrast, excitationPos, excitationNeg, excitationBackground] = extractResultsFromAcquisition(acquisition(i));
-    acquisitionResults(i).contrast = contrast';
-    acquisitionResults(i).excitationPos = excitationPos';
-    acquisitionResults(i).excitationNeg = excitationNeg';
-    acquisitionResults(i).excitationBackground = excitationBackground';
+    acquisitionResults = extractResultsFromAcquisition(acquisition(i));
 end
-
-%% Convert to table
-acquisitionResults = struct2table(acquisitionResults);
-acquisitionResults = splitvars(acquisitionResults,'contrast','NewVariableNames',{'contrast_L','contrasts_M','contrast_S','contrast_Mel'});
-acquisitionResults = splitvars(acquisitionResults,'excitationPos','NewVariableNames',{'excitationPos_L','excitationPos_M','excitationPos_S','excitationPos_Mel'});
-acquisitionResults = splitvars(acquisitionResults,'excitationNeg','NewVariableNames',{'excitationNeg_L','excitationNeg_M','excitationNeg_S','excitationNeg_Mel'});
-acquisitionResults = splitvars(acquisitionResults,'excitationBackground','NewVariableNames',{'excitationBackground_L','excitationBackground_M','excitationBackground_S','excitationBackground_Mel'});
 
 %% Save
 outputFileDir = fileparts(outputFilePath);
-if ~isdir(outputFileDir)
+if ~isfolder(outputFileDir)
     mkdir(outputFileDir)
 end
 writetable(acquisitionResults,outputFilePath);

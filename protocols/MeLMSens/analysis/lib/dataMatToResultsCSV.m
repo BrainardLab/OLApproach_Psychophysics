051d0de55d2951487f6529416bbd38729111aa14
Joris Vincent <joris.vincent@gmail.com>
2018-10-28 11:25:01 -0400

    Add `datafile` column to csv

--- protocols/MeLMSens/analysis/lib/dataMatToResultsCSV.m
+++ protocols/MeLMSens/analysis/lib/dataMatToResultsCSV.m
@@ -1,12 +1,14 @@
 function dataMatToResultsCSV(inputFilePath, outputFilePath)
 %% Load raw datafile
 load(inputFilePath, 'acquisition');
+[~, inputFileName] = fileparts(inputFilePath);
 
 %% Extract results from raw datafile
 acquisitionResults = table();
 for i = 1:numel(acquisition)
     acquisitionResults = extractResultsFromAcquisition(acquisition(i));
 end
+acquisitionResults = addvarString(acquisitionResults,inputFileName,'VariableName','datafile');
 
 %% Save
 outputFileDir = fileparts(outputFilePath);

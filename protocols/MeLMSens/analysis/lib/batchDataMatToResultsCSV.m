5f040c1ba702401a007c14d3290054563553856d
Joris Vincent <joris.vincent@gmail.com>
2018-10-28 11:27:18 -0400

    Skip `pilot` sessions

--- protocols/MeLMSens/analysis/lib/batchDataMatToResultsCSV.m
+++ protocols/MeLMSens/analysis/lib/batchDataMatToResultsCSV.m
@@ -1,6 +1,9 @@
 matfiles = dir('../../data/raw/*/*/data-*.mat');
 for m = 1:numel(matfiles)
     dirEntry = matfiles(m);
+    if contains(dirEntry.name,'pilot')
+        continue;
+    end
     inputFile = fullfile(dirEntry.folder,dirEntry.name);
     outputFile = replace(inputFile,'raw','processed');
     outputFile = replace(outputFile,'.mat','.csv');

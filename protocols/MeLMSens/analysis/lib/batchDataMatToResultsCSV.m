4fc52ba4708f65f942787e35b2381d6c2f24e7f9
Joris Vincent <joris.vincent@gmail.com>
2018-10-28 11:27:29 -0400

    More verbose

--- protocols/MeLMSens/analysis/lib/batchDataMatToResultsCSV.m
+++ protocols/MeLMSens/analysis/lib/batchDataMatToResultsCSV.m
@@ -1,7 +1,13 @@
 matfiles = dir('../../data/raw/*/*/data-*.mat');
+fprintf('Found files:\n');
+fprintf('\t%s\n',matfiles.name);
+
+fprintf('\n');
 for m = 1:numel(matfiles)
     dirEntry = matfiles(m);
+    fprintf('Processing file %s...',dirEntry.name);
     if contains(dirEntry.name,'pilot')
+        fprintf('pilot file; skipped.\n');
         continue;
     end
     inputFile = fullfile(dirEntry.folder,dirEntry.name);
@@ -9,4 +15,5 @@ for m = 1:numel(matfiles)
     outputFile = replace(outputFile,'.mat','.csv');
     outputFile = replace(outputFile,'data-','results_acquisition-');
     dataMatToResultsCSV(inputFile,outputFile);
+    fprintf('saved to %s.\n',outputFile);
 end

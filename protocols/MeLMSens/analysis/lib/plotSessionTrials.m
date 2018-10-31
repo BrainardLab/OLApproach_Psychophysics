60639c5249551a1132d18d01fb0eb7b47fa3e5c8
Joris Vincent <joris.vincent@gmail.com>
2018-10-31 10:41:52 -0400

    Get rid of individual psychometric function plots

--- protocols/MeLMSens/analysis/lib/plotSessionTrials.m
+++ protocols/MeLMSens/analysis/lib/plotSessionTrials.m
@@ -6,10 +6,12 @@ function F = plotSessionTrials(session)
 %   F = plotSessionTrials(session)
 %
 % Description:
-%       % Four acquisitions per session: Mel high/low, LMS high/low. High and low
-% will be plot next to each other on the same row of plots; Mel on the top
-% row, LMS on the bottom. Each acquisition gets 2 plots: staircases in one, psychometric function
-% fits in the other.
+%	 Plot the staircases and psychometric function fits for session. Left
+%	 hand side will plot stairces for the four acquisitions per session:
+%	 Mel high/low, LMS high/low. High and low will be plot next to each
+%	 other on the same row of plots; Mel on the top row, LMS on the bottom
+%    Right hand panel will plot one large plot with all four psychometric
+%    function fits.
 %
 % Inputs:
 %    session - containers.Map() containing acquisitions under the keys
@@ -39,14 +41,10 @@ LMS_high = session('LMS_high');
 % Each acquisition gets 2 plots: staircases in one, psychometric function
 % fits in the other.
 % Thus:
-Mel_low_staircases      = subplot(2,8,1);
-Mel_low_psychometric    = subplot(2,8,2);
-Mel_high_staircases     = subplot(2,8,3);
-Mel_high_psychometric   = subplot(2,8,4);
-LMS_low_staircases      = subplot(2,8,9);
-LMS_low_psychometric    = subplot(2,8,10);
-LMS_high_staircases     = subplot(2,8,11);
-LMS_high_psychometric	= subplot(2,8,12);
+Mel_low_staircases      = subplot(2,4,1);
+Mel_high_staircases     = subplot(2,4,2);
+LMS_low_staircases      = subplot(2,4,5);
+LMS_high_staircases     = subplot(2,4,6);
 
 % Plot staircases
 % Ask the acquisitions to plot the staircases in the specified axes. Update
@@ -56,13 +54,17 @@ Mel_high.plotStaircases('ax',Mel_high_staircases); title('Mel high staircases');
 LMS_low.plotStaircases('ax',LMS_low_staircases); title('LMS low staircases');
 LMS_high.plotStaircases('ax',LMS_high_staircases); title('LMS high staircases');
 
-% Plot each psychometric function
-% Ask the acquisitions to plot the psychometric functions in the specified 
-% axes. Update the titles to indicate the acquisition name.
-Mel_low.plotPsychometricFunction('ax',Mel_low_psychometric); title('Mel low psychometric function');
-Mel_high.plotPsychometricFunction('ax',Mel_high_psychometric); title('Mel high psychometric function');
-LMS_low.plotPsychometricFunction('ax',LMS_low_psychometric); title('LMS low psychometric function');
-LMS_high.plotPsychometricFunction('ax',LMS_high_psychometric); title('LMS high psychometric function');
+% % Plot each psychometric function
+% % Ask the acquisitions to plot the psychometric functions in the specified 
+% % axes. Update the titles to indicate the acquisition name.
+% Mel_low_psychometric    = subplot(2,4,2);
+% Mel_high_psychometric   = subplot(2,4,4);
+% LMS_low_psychometric    = subplot(2,4,10);
+% LMS_high_psychometric	= subplot(2,4,12);
+% Mel_low.plotPsychometricFunction('ax',Mel_low_psychometric); title('Mel low psychometric function');
+% Mel_high.plotPsychometricFunction('ax',Mel_high_psychometric); title('Mel high psychometric function');
+% LMS_low.plotPsychometricFunction('ax',LMS_low_psychometric); title('LMS low psychometric function');
+% LMS_high.plotPsychometricFunction('ax',LMS_high_psychometric); title('LMS high psychometric function');
 
 % Plot combined psychometric functions
 ax_psychometricFunctions = subplot(1,2,2);

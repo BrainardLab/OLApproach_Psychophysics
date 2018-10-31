b778d2cccc0838299a358e0d344daa5eac4f8936
Joris Vincent <joris.vincent@gmail.com>
2018-10-31 15:00:51 -0400

    listParticipants & listSessions
    To make some of the file management easier

---
+++ protocols/MeLMSens/analysis/lib/listSessions.m
@@ -0,0 +1,9 @@
+function sessions = listSessions(participant)
+%LISTSESSIONS Summary of this function goes here
+%   Detailed explanation goes here
+dataRawPath = getpref('MeLMSens','ProtocolDataRawPath');
+FSEntries = dir(fullfile(dataRawPath,participant,'*session*'));
+directories = FSEntries([FSEntries.isdir]);
+directories = directories(~strcmp({directories.name},{'..'}) & ~strcmp({directories.name},{'.'}));
+sessions = {directories.name};
+end

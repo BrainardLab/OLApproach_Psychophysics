5c6cbf80bcbd44ddc69aabf3d3fc1cb423681211
Joris Vincent <joris.vincent@gmail.com>
2018-11-15 17:29:34 -0500

    addvarString now needs to get variable headings as cellstr

--- protocols/MeLMSens/analysis/lib/extractValidationTablesFromMaterials.m
+++ protocols/MeLMSens/analysis/lib/extractValidationTablesFromMaterials.m
@@ -10,9 +10,9 @@ contrastsFlicker = [];
 
 if isfield(materials,'validationsPre')
     [luminancesBgPre, contrastsBgPre, contrastsFlickerPre] = validationsToTablesMeLMSens_SteadyAdapt(materials.validationsPre);
-    luminancesBgPre = addvarString(luminancesBgPre,'PreCorrection','VariableName','label');
-    contrastsBgPre = addvarString(contrastsBgPre,'PreCorrection','VariableName','label');
-    contrastsFlickerPre = addvarString(contrastsFlickerPre,'PreCorrection','VariableName','label');
+    luminancesBgPre = addvarString(luminancesBgPre,'PreCorrection','VariableName',"label");
+    contrastsBgPre = addvarString(contrastsBgPre,'PreCorrection','VariableName',"label");
+    contrastsFlickerPre = addvarString(contrastsFlickerPre,'PreCorrection','VariableName',"label");
     luminancesBg = vertcat(luminancesBg, luminancesBgPre);
     contrastsBg = vertcat(contrastsBg, contrastsBgPre);
     contrastsFlicker = vertcat(contrastsFlicker, contrastsFlickerPre);
@@ -20,9 +20,9 @@ end
 
 if isfield(materials,'validationsPostCorrection')
     [luminancesBgPostCorrection, contrastsBgPostCorrection, contrastsFlickerPostCorrection] = validationsToTablesMeLMSens_SteadyAdapt(materials.validationsPostCorrection);
-    luminancesBgPostCorrection = addvarString(luminancesBgPostCorrection,'PostCorrection','VariableName','label');
-    contrastsBgPostCorrection = addvarString(contrastsBgPostCorrection,'PostCorrection','VariableName','label');
-    contrastsFlickerPostCorrection = addvarString(contrastsFlickerPostCorrection,'PostCorrection','VariableName','label');
+    luminancesBgPostCorrection = addvarString(luminancesBgPostCorrection,'PostCorrection','VariableName',"label");
+    contrastsBgPostCorrection = addvarString(contrastsBgPostCorrection,'PostCorrection','VariableName',"label");
+    contrastsFlickerPostCorrection = addvarString(contrastsFlickerPostCorrection,'PostCorrection','VariableName',"label");
     luminancesBg = vertcat(luminancesBg, luminancesBgPostCorrection);
     contrastsBg = vertcat(contrastsBg, contrastsBgPostCorrection);
     contrastsFlicker = vertcat(contrastsFlicker, contrastsFlickerPostCorrection);
@@ -30,9 +30,9 @@ end
 
 if isfield(materials,'validationsPostSession')
     [luminancesBgPostSession, contrastsBgPostSession, contrastsFlickerPostSession] = validationsToTablesMeLMSens_SteadyAdapt(materials.validationsPostSession);
-    luminancesBgPostSession = addvarString(luminancesBgPostSession,'PostSession','VariableName','label');
-    contrastsBgPostSession = addvarString(contrastsBgPostSession,'PostSession','VariableName','label');
-    contrastsFlickerPostSession = addvarString(contrastsFlickerPostSession,'PostSession','VariableName','label');
+    luminancesBgPostSession = addvarString(luminancesBgPostSession,'PostSession','VariableName',"label");
+    contrastsBgPostSession = addvarString(contrastsBgPostSession,'PostSession','VariableName',"label");
+    contrastsFlickerPostSession = addvarString(contrastsFlickerPostSession,'PostSession','VariableName',"label");
     luminancesBg = vertcat(luminancesBg, luminancesBgPostSession);
     contrastsBg = vertcat(contrastsBg, contrastsBgPostSession);
     contrastsFlicker = vertcat(contrastsFlicker, contrastsFlickerPostSession);

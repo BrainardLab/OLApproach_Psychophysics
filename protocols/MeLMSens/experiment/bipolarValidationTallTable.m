423a7e8e18a028480ba2fca49f6f2c9eedd38e49
Joris Vincent <joris.vincent@gmail.com>
2018-11-15 17:27:58 -0500

    Flicker contrasts tall table: separate rows positive/negative component

--- protocols/MeLMSens/experiment/bipolarValidationTallTable.m
+++ protocols/MeLMSens/experiment/bipolarValidationTallTable.m
@@ -4,13 +4,17 @@ function T = bipolarValidationTallTable(validation)
 receptorStrings = {'L','M','S','Mel'}';
 time = repmat(validation.time(1),[length(receptorStrings),1]);
 
-contrastDesired = validation.contrastDesired; % desired modulation, not differential, contrast
-contrastDesired = table(time, receptorStrings,round(contrastDesired*100,2),...
-                    'VariableNames',{'time','receptor','desired'}); % convert to table
+% contrastDesired = validation.contrastDesired; % desired modulation, not differential, contrast
+% contrastDesired = table(time, receptorStrings,round(contrastDesired*100,2),...
+%                     'VariableNames',{'time','receptor','desired'}); % convert to table
 
 contrastActual = validation.contrastActual; % measured modulation, not differential, contrast
 contrastActual = table(time, receptorStrings, contrastActual*100,...
-                    'VariableNames',{'time','receptor','actual'});
-T = join(contrastDesired, contrastActual);
+                    'VariableNames',{'time','receptor','contrast'});
+contrastActual = splitvars(contrastActual, 'contrast','NewVariableNames',{'positive','negative'});
+contrastActual = stack(contrastActual,{'positive','negative'},'IndexVariableName','component','NewDataVariableName','contrast');
+
+%T = join(contrastDesired, contrastActual);
+T = contrastActual;
 T.receptor = categorical(T.receptor);
 end

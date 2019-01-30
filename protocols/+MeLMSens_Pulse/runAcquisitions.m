3fc93f68142e2ae1b6068a217f08968e2cec403e
Joris Vincent <joris.vincent@gmail.com>
2019-01-30 12:59:23 -0500

    Progress notifications per block

--- protocols/+MeLMSens_Pulse/runAcquisitions.m
+++ protocols/+MeLMSens_Pulse/runAcquisitions.m
@@ -11,20 +11,42 @@ for block = acquisitions % loop over columns
     fprintf('Running acquisition(s)...\n')
     
     abort = false;
-    while ~abort && ~isempty(block)
-        i = randi(numel(block));
-        if ~block(i).hasNextTrial()
-            block(i) = [];
-        else
-            [~, abort] = block(i).runNextTrial(oneLight, trialResponseSys);   
-        end
+    [progressTotal, progress] = getBlockProgress(block);
+    remainingAcquisitions = block(progress < 1);
+    while ~abort && progressTotal < 1
+        % Draw random acquisition from remainingAcquisitions
+        i = randi(numel(remainingAcquisitions));
+        
+        % Run trial
+        [~, abort] = remainingAcquisitions(i).runNextTrial(oneLight, trialResponseSys);   
+        
+        % Determine new block progress
+        [progressTotal, progress, NTrialsRemaining] = getBlockProgress(block);
+        
+        % Print block progress
+        fprintf('Block progress %.1f%% ([',progressTotal*100);
+        fprintf('%.1f%% ', progress*100);
+        fprintf(']): %.0f trials remaining\n',sum(NTrialsRemaining));
     end
     if abort
-        fprintf('Acquisition(s) interrupted.\n'); Speak('Acquisition interrupted.',[],230);
+        fprintf('Acquisitions interrupted.\n'); Speak('Acquisitions interrupted.',[],230);
+        return;
     else
-        fprintf('Acquisition(s) complete.\n'); Speak('Acquisition complete.',[],230);
+        fprintf('Block complete.\n'); Speak('Block complete.',[],230);        
     end
 end    
+fprintf('All acquisitions complete.\n'); Speak('All acquisitions complete.',[],230);
     
+end
+
+function [progressTotal, progress, NTrialsRemaining] = getBlockProgress(block)
+    NTrials = [];
+    NTrialsRemaining = [];
+    for acquisition = block'
+        NTrials = [NTrials acquisition.NTrialsPerStaircase * acquisition.NInterleavedStaircases];
+        NTrialsRemaining = [NTrialsRemaining sum(acquisition.nTrialsRemaining)];
+    end
+    progress = 1-(NTrialsRemaining./NTrials);
+    progressTotal = mean(progress);
 end
 

581962c7cca0a5930b356dd2ee447ce179fc7825
Joris Vincent <joris.vincent@pennmedicine.upenn.edu>
2019-02-11 17:42:45 -0500

    Update getCalibration

--- protocols/+MeLMSens_Pulse/getCalibration.m
+++ protocols/+MeLMSens_Pulse/getCalibration.m
@@ -2,7 +2,7 @@ function calibration = getCalibration()
 % Get calibration for the protocol
 % Specify which box and calibration to use, check that everything is set up
 % correctly, and retrieve the calibration structure.
-boxName = 'BoxB';
-calibrationType = 'BoxBLiquidLightGuideEyePiece3Beamsplitter';
+boxName = 'BoxD';
+calibrationType = 'BoxDLiquidLightGuidEyePiece3Beamsplitter';
 calibration = OLGetCalibrationStructure('CalibrationType',calibrationType,'CalibrationDate','latest');
 end

2e9c234cc9c6908411139a444e864819dc10fbbd
Joris Vincent <joris.vincent@gmail.com>
2019-02-27 13:43:20 -0500

    plot.sessionFigureFromName

---
+++ analysis/+MeLMSens_Pulse/+plot/sessionFigureFromName.m
@@ -0,0 +1,23 @@
+function F = sessionFigureFromName(participant, sessionName)
+%SESSIONFIGUREFROMNAME Summary of this function goes here
+%   Detailed explanation goes here
+
+% History:
+%    2019.02.26  jv   copied and adapted for MeLMSens_Pulse. Extracted
+%                     plot.export
+
+% Get data
+acquisitions = MeLMSens_Pulse.dataManagement.loadSessionAcquisitionsFromName(participant,sessionName);
+
+% Create figure
+F = MeLMSens_Pulse.plot.sessionFigure(acquisitions);
+
+% Add title
+t = annotation(F,'textbox',...
+    [0.4 0.97 0.20 0.05],...
+    'String',strrep(sprintf('%s %s',participant, sessionName),'_',' '),...
+    'FitBoxToText','on',...
+    'HorizontalAlignment','center',...
+    'FontSize',25,...
+    'LineStyle','none');
+end

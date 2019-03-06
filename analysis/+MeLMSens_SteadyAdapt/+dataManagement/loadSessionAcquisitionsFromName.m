796184537af0230041b70f58ee8d16485f38385c
Joris Vincent <joris.vincent@gmail.com>
2019-03-06 12:33:52 -0500

    loadSessionAcquisitionsFromName

--- analysis/+MeLMSens_SteadyAdapt/+dataManagement/loadSessionAcquisitionsFromName.m
+++ analysis/+MeLMSens_SteadyAdapt/+dataManagement/loadSessionAcquisitionsFromName.m
@@ -1,6 +1,6 @@
 function [acquisitions, metadata] = loadSessionAcquisitionsFromName(participant,sessionName)
 %LOADSESSIONFROMNAME Summary of this function goes here
 %   Detailed explanation goes here
-sessionPath = sessionPathFromName(participant, sessionName);
-[acquisitions, metadata] = loadSessionAcquisitionsFromPath(sessionPath);
+sessionPath = MeLMSens_SteadyAdapt.dataManagement.sessionRawPathFromName(participant, sessionName);
+[acquisitions, metadata] = MeLMSens_SteadyAdapt.dataManagement.loadSessionAcquisitionsFromPath(sessionPath);
 end

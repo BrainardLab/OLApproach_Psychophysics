304a69d3fc3c01229b33ed90ebcccd06d18c0daa
Joris Vincent <joris.vincent@gmail.com>
2019-03-06 15:01:51 -0500

    Fitted threshold, JND

---
+++ analysis/+MeLMSens_SteadyAdapt/+analyze/getThresholdFitDirection.m
@@ -0,0 +1,53 @@
+function thresholdDirection = getThresholdDirection(acquisition)
+% Get the nominal flicker direction shown at threshold
+%   
+% Syntax:
+%   thresholdDirection = getThresholdDirection(acquisition)
+%
+% Description:
+%    Return the direction nominally corresponding to the stimulus at
+%    threshold. Direction is scaled by the nominal threshold estimate.
+%   
+% Inputs:
+%    acquisition         - scalar MeLMSense_Pulse_acquisition
+%
+% Outputs:
+%    thresholdDirection  - scalar OLDirection_Bipolar, the flicker
+%                          direction for this acquisition scaled to nominal
+%                          LMS threshold contrast
+%
+% Optional keyword arguments:
+%    None.
+%
+% See also:
+%    MeLMSens_Pulse.acquisition.threshold,
+%    MeLMSens_Pulse.acquisition.flickerDirection,
+%    OLDirection_Bipolar.ScaleToReceptorContrast
+
+% History:
+%    2018.10.26  jv   wrote MeLMSens_SteadyAdapt.
+%                     extractResultsFromAcquisition with collection
+%                     of subfunctions
+%    2019.02.28  jv   extracted MeLMSens_Pulse. analyze.
+%                     getThresholdDirection
+%                     from
+%                     MeLMSens_SteadyAdapt. extractResultsFromAcquisition
+%    2019.03.06  jv   adapted for MeLMSens_SteadyAdapt, fitted threshold
+
+
+% Get direction, background
+direction = acquisition.direction;
+background = acquisition.background;
+
+% Get receptors
+receptors = acquisition.receptors;
+
+% Get nominal threshold contrast
+thresholdContrastFit = acquisition.fitPsychometricFunctionThreshold();
+
+% Convert to target scaling receptor contrasts
+targetContrasts = thresholdContrastFit * [1 1 1 0; -1 -1 -1 0]';
+
+% Scale direction to target contrast with background
+thresholdDirection = direction.ScaleToReceptorContrast(background,receptors,targetContrasts);
+end

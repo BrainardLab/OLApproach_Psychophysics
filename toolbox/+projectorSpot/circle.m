678d268b0f03897647fbaae328a93e846825b215
Joris Vincent <joris.vincent@gmail.com>
2019-04-05 16:14:02 -0400

    Can now draw Circle-objects directly on projector window

--- toolbox/+projectorSpot/circle.m
+++ toolbox/+projectorSpot/circle.m
@@ -8,6 +8,92 @@ classdef circle < handle
         center = [0 0];
         diameter = 100; 
     end
+    properties
+        window;
+    end
+    properties (Dependent)
+        isDrawn;
+        Visible;
+    end
+    
+    methods
+        % isDrawn == does the window have this object in queue?
+        function isDrawn = get.isDrawn(obj)
+            if isempty(obj.window)
+                isDrawn = false;
+            else
+                isDrawn = obj.window.findObjectIndex(obj.name) ~= -1;
+            end
+        end
+        function set.isDrawn(obj,draw)
+            if draw
+                obj.window.addOval(obj.center, [obj.diameter obj.diameter], obj.RGB,'Name',obj.name);
+            else
+                obj.window.deleteObject(obj.name);
+            end
+            obj.window.draw();
+        end
+        
+        % Visible
+        function Visible = get.Visible(obj)
+            if ~obj.isDrawn
+                Visible = false;
+            else
+                Visible = obj.window.getObjectProperty(obj.name,'Enabled');
+            end
+        end
+        function set.Visible(obj,visible)
+            if obj.isDrawn
+                obj.window.getObjectProperty(obj.name,'Enabled',false);
+            end
+            obj.Visible = visible;
+        end
+        
+        % RGB
+        function RGB = get.RGB(obj)
+            if obj.isDrawn
+                obj.RGB = obj.window.getObjectProperty(obj.name,'Color');
+            end
+            RGB = obj.RGB;
+        end
+        function set.RGB(obj, RGB)
+            if obj.isDrawn
+                obj.window.setObjectProperty(obj.name,'Color',RGB)
+                obj.window.draw();
+            end
+            obj.RGB = RGB;            
+        end
+        
+        % Center
+        function center = get.center(obj)
+            if obj.isDrawn
+                obj.center = obj.window.getObjectProperty(obj.name,'Center');
+            end
+            center = obj.center;
+        end
+        function set.center(obj, center)
+            if obj.isDrawn
+                obj.window.setObjectProperty(obj.name,'Center',center)
+                obj.window.draw();
+            end
+            obj.center = center;            
+        end  
+        
+        % Diameter
+        function diameter = get.diameter(obj)
+            if obj.isDrawn
+                obj.diameter = mean(obj.window.getObjectProperty(obj.name,'Dimensions'));
+            end
+            diameter = obj.diameter;
+        end
+        function set.diameter(obj, diameter)
+            if obj.isDrawn
+                obj.window.setObjectProperty(obj.name,'Dimensions',[diameter diameter])
+                obj.window.draw();
+            end
+            obj.diameter = diameter;            
+        end
+    end
     
     methods
         function obj = circle(varargin)
@@ -32,18 +118,24 @@ classdef circle < handle
             end
         end
         
-        function draw(obj,window)      
+        function draw(obj,window)
+            obj.window = window;
+            
             % Does the circle already exist? If not, add it.
-            if window.findObjectIndex(obj.name) == -1
-                window.addOval(obj.center, [obj.diameter obj.diameter], obj.RGB,'Name',obj.name);
+            if obj.window.findObjectIndex(obj.name) == -1
+                obj.window.addOval(obj.center, [obj.diameter obj.diameter], obj.RGB,'Name',obj.name);
             end
             
             % Set properties
-            window.setObjectProperty(obj.name,'Dimensions',[obj.diameter, obj.diameter]);
-            window.setObjectProperty(obj.name,'Center',obj.center);
-            window.setObjectProperty(obj.name,'Color',obj.RGB);
-            window.setObjectProperty(obj.name,'Name',obj.name);
-            window.draw();
+            obj.window.setObjectProperty(obj.name,'Dimensions',[obj.diameter, obj.diameter]);
+            obj.window.setObjectProperty(obj.name,'Center',obj.center);
+            obj.window.setObjectProperty(obj.name,'Color',obj.RGB);
+            obj.window.setObjectProperty(obj.name,'Name',obj.name);
+            obj.window.draw();
+        end
+        
+        function delete(obj)
+            obj.isDrawn = false;
         end
     end
 end

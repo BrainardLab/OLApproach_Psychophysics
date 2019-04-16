9e356dba0deae3a83c09fb6523ca4016f2cf3a67
Joris Vincent <joris.vincent@gmail.com>
2019-04-16 16:11:55 -0400

    Repeat measurements of RGB

--- toolbox/+projectorSpot/measureRGB.m
+++ toolbox/+projectorSpot/measureRGB.m
@@ -1,20 +1,33 @@
-function measurement = measureRGB(windowObject, RGB, radiometer)
+function measurements = measureRGB(windowObject, RGB, radiometer, NMeasurements)
 %MEASURESINGLELEVEL Summary of this function goes here
 %   Detailed explanation goes here
 
 windowObject.Visible = true;
 windowObject.RGB = RGB;
 
-measurement.measurable = struct(windowObject);
-
-SPD = radiometer.measure();
-
-measurement.SPD = SPD;
-measurement.wavelengths = MakeItWls(radiometer.userS);
-
 radiometerInfo = radiometer.currentConfiguration;
 radiometerInfo.model = radiometer.deviceModelName;
 radiometerInfo.serial = radiometer.deviceSerialNum;
 
-measurement.radiometerInfo = radiometerInfo;
+measurements = struct([]);
+for i = 1:NMeasurements
+    % Initialize measurement output
+    measurement = struct([]);
+    
+    % Store measurement
+    measurement.SPD = radiometer.measure();
+    measurement.wavelengths = MakeItWls(radiometer.userS);
+
+    % Timestamp
+    measurement.time = datetime();    
+    
+    % Store metadata about what's being measured
+    measurement.measurable = struct(windowObject);    
+
+    % Add radiometer metadata
+    measurement.radiometerInfo = radiometerInfo;
+    
+    % Append
+    measurements = [measurements; measurement];
+end
 end

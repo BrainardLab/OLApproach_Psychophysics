81cbc250778b16bdafde868ae7246f54d2a585d6
Joris Vincent <joris.vincent@gmail.com>
2019-04-18 14:47:55 -0400

    add makeAcquisition to test

--- protocols/+MeLMSens_Pulse2/test.m
+++ protocols/+MeLMSens_Pulse2/test.m
@@ -13,6 +13,7 @@ acquisition.background = directions('Mel_low');
 acquisition.pedestalDirection = directions('MelStep');
 acquisition.pedestalPresent = 1;
 acquisition.makeModulations();
+acquisition.staircase = MeLMSens_Pulse2.makeStaircase();
 
 %% Dummy stimulus
 dummyStim = acquisition.dummyStimulus();

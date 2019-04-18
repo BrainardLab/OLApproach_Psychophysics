8783efcb0f957d76fa69bb9af044cd584996429b
Joris Vincent <joris.vincent@gmail.com>
2019-04-18 16:37:44 -0400

    Validate and correct OLDirections

--- protocols/+MeLMSens_Pulse2/test.m
+++ protocols/+MeLMSens_Pulse2/test.m
@@ -6,19 +6,47 @@ calibration = getCalibration();
 
 %% Make directions
 directions = MeLMSens_Pulse2.makeNominalOLDirections(calibration);
+receptors = directions('MelStep').describe.directionParams.T_receptors;
 
-%% Setup acquisition
-acquisition = MeLMSens_Pulse2.Acquisition();
-acquisition.background = directions('Mel_low');
-acquisition.pedestalDirection = directions('MelStep');
-acquisition.pedestalPresent = 1;
-acquisition.makeModulations();
-acquisition.staircase = MeLMSens_Pulse2.makeStaircase();
+%% Validate nominal
+pSpot.hide();
+validationsNominal = MeLMSens_Pulse2.validateDirections(directions,oneLight,radiometer,...
+                                                'receptors',receptors,...
+                                                'primaryTolerance',1e-5,...
+                                                'nValidations',5,...
+                                                'temperatureProbe',temperatureProbe);
+
+%% Correct
+pSpot.hide();
+corrections = MeLMSens_Pulse2.correctDirections(directions,oneLight,calibration,radiometer,...
+                            receptors,...
+                            'smoothness',.001,...
+                            'temperatureProbe',temperatureProbe);
+
+%% Validate pre
+pSpot.hide();
+validationsPre = MeLMSens_Pulse2.validateDirections(directions,oneLight,radiometer,...
+                                                'receptors',receptors,...
+                                                'primaryTolerance',1e-5,...
+                                                'nValidations',5,...
+                                                'temperatureProbe',temperatureProbe);
+
+%% Make acquisitions
+acquisitions = MeLMSens_Pulse2.makeAcquisitions(...
+                directions,...
+                receptors,...
+                'NTrialsPerStaircase',3);
 
 %% Get trialResponseSys
 trialResponseSys = getTrialResponseSystem(gamePad);
 
-%% Test trial
-trial = acquisition.makeTrial(10);
+%% Run acquisitions
+trialResponseSys.waitForResponse();
+MeLMSens_Pulse2.runAcquisitions(acquisitions,oneLight,pSpot,trialResponseSys);
 
-trial.run(oneLight,pSpot,trialResponseSys);
+%% Validations post
+pSpot.hide();
+validationsPost = MeLMSens_Pulse2.validateDirections(directions,oneLight,radiometer,...
+                                                'receptors',receptors,...
+                                                'primaryTolerance',1e-5,...
+                                                'nValidations',5);

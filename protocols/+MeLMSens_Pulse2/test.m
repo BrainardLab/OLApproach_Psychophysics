20dcc5cb26225f14a992403f5caaef136b4add03
Joris Vincent <joris.vincent@gmail.com>
2019-04-18 13:01:59 -0400

    Simple test script

---
+++ protocols/+MeLMSens_Pulse2/test.m
@@ -0,0 +1,20 @@
+%% Get projector spot
+pSpot = projectorSpot.getProjectorSpot();
+
+%% Get calibration
+calibration = getCalibration();
+
+%% Make directions
+directions = MeLMSens_Pulse2.makeNominalOLDirections(calibration);
+
+%% Setup acquisition
+acquisition = MeLMSens_Pulse2.Acquisition();
+acquisition.background = directions('Mel_low');
+acquisition.pedestalDirection = directions('MelStep');
+acquisition.pedestalPresent = 1;
+acquisition.makeModulations();
+
+%% Dummy stimulus
+dummyStim = acquisition.dummyStimulus();
+
+dummyStim.show(oneLight,pSpot);

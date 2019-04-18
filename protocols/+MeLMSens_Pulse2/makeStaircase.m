6ca77e02f0b954edecd427573f871487a3b395a1
Joris Vincent <joris.vincent@gmail.com>
2019-04-18 16:25:32 -0400

    makeStaircase input argument NTrialsPerStaircase

--- protocols/+MeLMSens_Pulse2/makeStaircase.m
+++ protocols/+MeLMSens_Pulse2/makeStaircase.m
@@ -1,10 +1,10 @@
-function staircase = makeStaircase()
+function staircase = makeStaircase(NTrialsPerStaircase)
 %MAKESTAIRCASE Summary of this function goes here
 %   Detailed explanation goes here
 staircase = Staircases.InterleavedStaircase;
 staircase.stimulusStep = 1;
 staircase.stimulusMin = 0;
 staircase.stimulusMax = 30;
-staircase.NTrialsPerStaircase = 40;
+staircase.NTrialsPerStaircase = NTrialsPerStaircase;
 staircase.stepSizes = [16 8 4 2 1];
 end

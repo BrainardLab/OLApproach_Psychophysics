f6d737f09515c5fab5c877ec25e03c028c7083da
Joris Vincent <joris.vincent@gmail.com>
2019-04-24 13:47:42 -0400

    InterleavedStaircase.plotTrialSeries

---
+++ toolbox/+Staircases/@InterleavedStaircase/plotTrialSeries.m
@@ -0,0 +1,24 @@
+function ax = plotTrialSeries(obj,varargin)
+% Plot all trials of this acquisition
+
+% Parse input
+parser = inputParser();
+parser.addRequired('obj');
+parser.addParameter('ax',gca,@(x) isgraphics(x) && strcmp(x.Type,'axes'));
+parser.parse(obj,varargin{:});
+ax = parser.Results.ax;
+axes(ax); hold on;
+
+% Plot staircases trialseries
+Staircases.Plot.stimulusLevelsCorrects(obj.stimulusLevels,obj.corrects,...
+                                        'ax',ax,...
+                                        'UserData',obj);
+
+% % Plot mean threshold
+% color = ax.ColorOrder(ax.ColorOrderIndex,:); % current plot color, which we'll reuse)
+% plot(xlim,obj.threshold*[1 1],'--','Color',color);
+% text(10,obj.threshold+0.001,...
+%     sprintf('Fit threshold = %.3f',mean(obj.threshold)),...
+%     'Color',color,...
+%     'FontWeight','bold');
+end

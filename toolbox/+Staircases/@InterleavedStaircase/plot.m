5a80f93c1c195b1373035586750426c1e077d95d
Joris Vincent <joris.vincent@gmail.com>
2019-04-24 14:07:47 -0400

    Plot threshold in InterleavedStaircase.plot()

--- toolbox/+Staircases/@InterleavedStaircase/plot.m
+++ toolbox/+Staircases/@InterleavedStaircase/plot.m
@@ -1,9 +1,19 @@
-function F = plot(obj)
+function F = plot(obj,varargin)
 %PLOT Summary of this function goes here
 %   Detailed explanation goes here
 
+% Parse input
+parser = inputParser();
+parser.addRequired('obj');
+parser.addParameter('threshold',[]);
+parser.addParameter('criterion',[]);
+parser.parse(obj,varargin{:});
+threshold = parser.Results.threshold;
+criterion = parser.Results.criterion;
+
+
 F = figure();
 
-obj.plotTrialSeries('ax',subplot(1,2,1));
-obj.plotProportionCorrect('ax',subplot(1,2,2));
+obj.plotTrialSeries('ax',subplot(1,2,1),'threshold',threshold);
+obj.plotProportionCorrect('ax',subplot(1,2,2),'threshold',threshold,'criterion',criterion);
 end
